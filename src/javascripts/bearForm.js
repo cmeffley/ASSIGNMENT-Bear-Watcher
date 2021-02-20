e61bf396547aa9f6903260817826c80b3eac26f5
Chris Meffley <cmeffley@gmail.com>
2021-02-20 14:56:29 -0500

    card populates and started see all bears

--- src/javascripts/bearForm.js
+++ src/javascripts/bearForm.js
@@ -1,4 +1,4 @@
-import bears from './river';
+// import bears from './river';
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.querySelector(divId);
@@ -12,22 +12,23 @@ const bearForm = () => {
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <form class="text-center">
+  <form id="form" class="text-center">
     <div class="mb-3">
     <label for="addNewBear" class="form-label">Add New Bear</label>
-    <input type="text" class="form-control" id="addBear" required>
+    <input type="text" class="form-control" id="addBear" placeholder="Name of Bear" required>
     <label for="basic-url" class="form-label">Add Picture</label>
-    <input type="text" class="form-control" id="bearPicture" required>
+    <input type="text" class="form-control" id="bearPicture" placeholder="Url" required>
     </div>
     <button type="submit" class="btn btn-primary" id="sort1">Submit</button>
     </form>
 </div>`;
-  printToDom('body', screen);
+  printToDom('#app', screen);
 };
 
 const formInfo = (e) => {
   e.preventDefault();
 
+  const newBears = [];
   const name = document.querySelector('#addBear').value;
   const picture = document.querySelector('#bearPicture').value;
 
@@ -35,27 +36,28 @@ const formInfo = (e) => {
     name,
     picture,
   };
-  bears.push(bearObj);
+  newBears.push(bearObj);
   // eslint-disable-next-line no-use-before-define
-  createBearCard(bears);
-  document.querySelector('screen').reset();
+  createBearCard(newBears);
+  // document.querySelector('#form').reset();
 };
 
 const createBearCard = (taco) => {
   let card = '';
   taco.forEach((element) => {
-    card += `<div class="card mb-3" style="width: 18rem;">
+    card += `<div class="card mb-3" style="width: 18rem;" id="card1">
     <div class="card-body text-center">
     <h2 class="card-text">${element.name}</h2>
     <img class="card-img-top" src="${element.picture}" alt="Picture of a bear">
   </div>
   </div>`;
   });
-  printToDom('#app', card);
+  printToDom('#app2', card);
 };
 
 const buttonActions = () => {
   document.querySelector('#sort1').addEventListener('click', formInfo);
+  document.querySelector('#all-bears').addEventListener('click');
 };
 
 export { bearForm, buttonActions };
--- src/javascripts/river.js
+++ src/javascripts/river.js
@@ -1,16 +1,20 @@
-const bears = [
-  {
-    name: 'Grizz',
-    picture: 'https://www.gannett-cdn.com/presto/2018/08/22/USAT/76151925-b45a-45b6-b62d-9fe0b3cc4c77-XXX_Trevor_Hughes_Yellowstone-Teton_August2018_2200.JPG?crop=5199,2960,x0,y1043&width=3200&height=1822&format=pjpg&auto=webp',
-  },
-  {
-    name: 'Bubba',
-    picture: 'https://static01.nyt.com/images/2020/09/30/multimedia/00xp-fat-bear-week3/00xp-fat-bear-week3-mobileMasterAt3x.jpg',
-  },
-  {
-    name: 'Popscicle',
-    picture: 'https://www.wildnatureimages.com/images/xl/180620-076-Brown-Bear_.jpg',
-  },
-];
+// const bears = [
+//   {
+//     name: 'Revel',
+//     picture: 'https://www.gannett-cdn.com/presto/2018/08/22/USAT/76151925-b45a-45b6-b62d-9fe0b3cc4c77-XXX_Trevor_Hughes_Yellowstone-Teton_August2018_2200.JPG?crop=5199,2960,x0,y1043&width=3200&height=1822&format=pjpg&auto=webp',
+//   },
+//   {
+//     name: 'Bubba',
+//     picture: 'https://static01.nyt.com/images/2020/09/30/multimedia/00xp-fat-bear-week3/00xp-fat-bear-week3-mobileMasterAt3x.jpg',
+//   },
+//   {
+//     name: 'Popscicle',
+//     picture: 'https://www.wildnatureimages.com/images/xl/180620-076-Brown-Bear_.jpg',
+//   },
+// ];
 
-export default bears;
+const seeAllBears = () => {
+  const seeBears = bears.map(newBears);
+}
+
+// export default bears;
